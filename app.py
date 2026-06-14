546229ff9db389062ac503f3e83e9d88cad5e1c5
TraineeNash <syafiq.nasrul106@gmail.com>
2026-06-14 17:17:58 +0800

    amend the path for flask

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from google import genai
 from flask import Flask,request,jsonify
+from flask import send_from_directory
+import os
 from flask_cors import CORS
 app = Flask(__name__)
 
 CORS(app,resources={r"/*": {"origins": "*"}})
 client = genai.Client(api_key="AQ.Ab8RN6I5C4yJMo9izH_L4PfiRkN3MaJT34kjHAuBOQohT5Pd8g")
+
+@app.route('/')
+def home():
+    return send_from_directory('.', 'index.html')
+
+@app.route('/<path:path>')
+def serve_file(path):
+    if os.path.exists(path):
+        return send_from_directory('.', path)
+    return "Not found", 404
 
 @app.route("/summary",methods=["POST"])
 def summary():
@@ -18,4 +30,4 @@ def summary():
     print("AI:", response.text)
     return jsonify({"summary": response.text})
 if __name__ == "__main__":
-    app.run(debug=True,host="0.0.0.0",port=5000)
+    app.run(host="0.0.0.0",port=5000)
